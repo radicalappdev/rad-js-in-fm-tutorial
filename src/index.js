b4bd2e60b141089e386dd04c615129a9be1c58ef
Joseph Simpson <joe@radicalappdev.com>
2022-09-09 06:57:14 -0400

    inlining an image via data url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 //Here we're importing items we'll need. You can add other imports here.
 import { helloModuleFunction } from "./modules/hello.js";
+
+import sampleImage from "data-url:./img/Sample.png";
 //The first function. Remove this.
 const btn = document.querySelector("button");
 btn.onclick = function () {
-  alert("You ran some JavaScript");
+  // alert("You ran some JavaScript");
+  var frame = document.getElementById("imageFrame");
+  if (frame !== null && frame !== undefined) {
+    frame.src = sampleImage;
+  }
 };
 
 //
